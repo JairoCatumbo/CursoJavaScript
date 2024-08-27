3422487b27130bde2d5ddf58698a52920e3df3c3
JairoCatumbo <jairoreceadocatumbo@gmail.com>
2024-08-27 21:47:54 +0100

    3-particularidades de js
    comentários, fraca tipagem, case sensitive, opcional( ; )

---
+++ aulas/3-particularidades/script.js
@@ -0,0 +1,17 @@
+console.log("primeiro");
+
+console.log("ultimo");
+
+console.log("mais um console.log");
+
+var a =1;
+
+a="teste";
+
+console.log(a);
+
+//comentario
+
+/* comentario de 
+multiplas 
+linhas*/ 
