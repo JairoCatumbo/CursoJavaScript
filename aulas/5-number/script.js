3d73676e140c3b685ef08622742735931af56148
JairoCatumbo <jairoreceadocatumbo@gmail.com>
2024-08-27 22:20:09 +0100

    5-number
    tipos de numeros:
    NaN
    +infinity
    -infinity

---
+++ aulas/5-number/script.js
@@ -0,0 +1,15 @@
+var numero=5;
+ 
+console.log(typeof numero);
+
+var float =5.32;
+console.log(float);
+console.log(typeof float);
+
+var testn="345";
+console.log(testn);
+console.log(typeof testn);
+console.log(NaN);
+console.log(typeof NaN);
+console.log( typeof +Infinity);
+console.log( typeof -Infinity);
