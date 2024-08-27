c3bed0264b23a7563db0c4502d62f04b28c39fd6
JairoCatumbo <jairoreceadocatumbo@gmail.com>
2024-08-27 22:02:21 +0100

    4-variaveis
    três formas de declarar variaveis:
    var meuPrimeiroNome;
    let nome5;
    const pi=3.14;

---
+++ aulas/4-variaveis/script.js
@@ -0,0 +1,18 @@
+var teste=1;
+
+console.log(teste);
+
+teste="jairo";
+
+console.log(teste);
+
+var meuPrimeiroNome="jairo";
+
+console.log(meuPrimeiroNome);
+
+let testando=1;
+const pi=3.14;
+
+console.log(testando);
+
+console.log(pi);
