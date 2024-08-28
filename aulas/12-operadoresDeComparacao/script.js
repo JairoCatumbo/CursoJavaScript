608c41a38bfe13fbb63f3d2011efecb07ec87572
JairoCatumbo <jairoreceadocatumbo@gmail.com>
2024-08-28 22:21:00 +0100

    12-operadoresComparacao
    operadores usados na matematica como:
    == igual
    <=menor ou igual que
    >= maior ou igual que
    < menor
    > maior

---
+++ aulas/12-operadoresDeComparacao/script.js
@@ -0,0 +1,18 @@
+var idade=18;
+
+if(idade>=18){
+    console.log("pode fazer a carteira");
+}
+if(idade<=17){
+    console.log("nao pode fazer a carteira");
+}
+
+var nome ="jair";
+
+if(nome =="jairo"){
+    console.log("ö seu nome e jairo" +nome);
+}
+
+if(nome !="jairo"){
+    console.log("o seu nome nao e jairo " +nome);
+}
