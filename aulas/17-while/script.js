ed84f9f7385055d17218c3b23de207cfebcbbba3
JairoCatumbo <jairoreceadocatumbo@gmail.com>
2024-08-28 23:28:06 +0100

    17-while
    estrutura de repetição emquanto executa o bloco

---
+++ aulas/17-while/script.js
@@ -0,0 +1,23 @@
+var x=0;
+
+while(x<5){
+    console.log("x=" +x);
+x++;
+}
+
+var arr = ['test','testando','a','b'];
+
+var y = 0;
+while(y <=3){
+    console.log(arr[y]);
+    y++;
+
+}
+
+var nome="jairo";
+
+var i=0;
+while(i<5){
+    console.log(nome[i]);
+    i++;
+}
