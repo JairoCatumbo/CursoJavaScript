8fdfcf1623675170cfdf51edefbd158df180b455
JairoCatumbo <jairoreceadocatumbo@gmail.com>
2024-08-28 22:49:47 +0100

    15-|| or
    executa o bloco caso uma das conparações seja verdadeira ou as duas, só nao entra caso as duas sejam falsas

---
+++ aulas/15-operador II/script.js
@@ -0,0 +1,12 @@
+var idade=15;
+var nome="jairo";
+
+if((nome=="jairo")||(idade>15)){
+    console.log("pode entrar: bem-vindo");
+}else {
+    console.log("nao pode entrar");
+}
+
+if((nome!="jairo")||(idade<16)){
+    console.log("nao pode entrar");
+}
