9c218da05dc840dfe6de7983c7b19a1c16674618
JairoCatumbo <jairoreceadocatumbo@gmail.com>
2024-08-28 23:44:33 +0100

    19-for
    deve seguir a ordem
    1-variavel com atribuicao
    2-condição
    3-incremento ou decremento
    for(var i=1; i<10;i++){
    
    }

---
+++ aulas/19-for/script.js
@@ -0,0 +1,12 @@
+var t=7;
+for(var i=0;i<12;i++){
+    console.log("7"+ i +" = " + t*i);
+
+}
+
+var nome="jairo";
+
+for(var c=0;c<nome.length;c++){
+    console.log(nome[c]);
+    document.write(nome[c]);
+}
