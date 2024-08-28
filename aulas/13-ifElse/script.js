b2b9a54634d91e949daaae1cac92d7de7ed183f6
JairoCatumbo <jairoreceadocatumbo@gmail.com>
2024-08-28 22:32:36 +0100

    13-ifElse
    estrutura de if alinhados com varias conparações

---
+++ aulas/13-ifElse/script.js
@@ -0,0 +1,12 @@
+var nome = "jai";
+
+if(nome == "jairo"){
+    console.log("o seu nome nao e jairo");
+}else if(nome == "jair"){
+    console.log("esqueceo o 'o' ficou:" +nome);
+}else{
+    console.log("o seu nome nao e jair nem jairo eh :" +nome);
+}
+
+
+
