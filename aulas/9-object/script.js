4908dde92a5ff721d904de4a9f1e63e37e34f236
JairoCatumbo <jairoreceadocatumbo@gmail.com>
2024-08-28 21:49:19 +0100

    9-objectos
    tipo de dados chave e valor, estrutura que armazena varios valores em uma so variavel "objecto"

---
+++ aulas/9-object/script.js
@@ -0,0 +1,12 @@
+var obj={
+    nome:"jairo",
+    idade:27,
+    sobrenome:"catumbo"
+};
+
+console.log(obj);
+console.log(typeof obj);
+
+console.log(obj.idade);
+console.log(obj.nome);
+console.log(obj.sobrenome);
