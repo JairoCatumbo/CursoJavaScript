a58b0c4b8ffb3bffe8b56a2545dd146e1f69b78b
JairoCatumbo <jairoreceadocatumbo@gmail.com>
2024-08-29 22:53:23 +0100

    23-escopo
    escopo de variaveis global em todo programa
    local apenas na funcao

---
+++ aulas/23-escolpo/script.js
@@ -0,0 +1,18 @@
+var x=1;
+var y=3;
+
+console.log(x,y);
+
+function teste(){
+    var z=0;
+    console.log(x,y,z);
+}
+
+teste();
+
+function testando(){
+    var z=5;
+    console.log(z);
+}
+
+testando();
