cfa47c29feae706b010770d531f49e98b9248032
JairoCatumbo <jairoreceadocatumbo@gmail.com>
2024-08-29 22:25:33 +0100

    21-funçoes
    criação de funções bloco de codigo que pode ser chamado em qualquer parte do programa

---
+++ aulas/21-funçoes/script.js
@@ -0,0 +1,26 @@
+function primeiraFuncao(){
+    console.log("ola mundo das funcoes");
+
+}
+
+primeiraFuncao();
+
+function dizerNome(nome){
+    console.log("o nome e: " + nome);
+}
+
+dizerNome("jairo");
+dizerNome("catumbo");
+
+var nomeDoBanco="aulas";
+
+dizerNome(nomeDoBanco);
+
+function soma(a,b){
+
+    return a+b;
+}
+
+var somaNumeros=soma(1,3);
+
+console.log(somaNumeros);
