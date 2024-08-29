d9bb70cb06390962944061d50576ee506b453e29
JairoCatumbo <jairoreceadocatumbo@gmail.com>
2024-08-29 23:19:19 +0100

    26-metodosde string1
    algums metodos para trabalhar com string

---
+++ aulas/26-metodosString1/script.js
@@ -0,0 +1,16 @@
+
+var nome="jairo";
+
+console.log(nome.length);
+
+console.log(nome[0]);
+
+console.log(nome.indexOf("iro"));
+
+var novoNome= nome.slice(nome.indexOf("iro"),nome.length);
+
+console.log(novoNome);
+
+var nnome=novoNome.replace("ro","roba")
+
+console.log(nnome);
