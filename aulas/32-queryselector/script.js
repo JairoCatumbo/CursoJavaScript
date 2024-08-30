279850eec037687f958af42fc84d11f532415408
JairoCatumbo <jairoreceadocatumbo@gmail.com>
2024-08-30 21:38:37 +0100

    32-queryselector
    nova forma de acessar elementos no DOM:
    querySelector
    querySelectorAll

---
+++ aulas/32-queryselector/script.js
@@ -0,0 +1,17 @@
+var itensClasse=document.getElementsByClassName('item');
+console.log(itensClasse);
+//queryselectorall
+var itensQuery=document.querySelectorAll('#lista2 li');
+console.log(itensQuery);
+
+var itensQuery2=document.querySelectorAll('#lista2 .item');
+console.log(itensQuery2);
+
+//queryselector funcona como o id
+
+var lista=document.querySelector('#lista');
+
+console.log(lista);
+
+var span=document.querySelector('#paragrafo span');
+console.log(span);
