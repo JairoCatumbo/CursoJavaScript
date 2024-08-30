b199f69e0d0b5170e24c071681e9eadb03f32218
JairoCatumbo <jairoreceadocatumbo@gmail.com>
2024-08-30 21:23:54 +0100

    31-acessandoODOM
    metodos de acessar o DOM:
    tagname
    id
    classe

---
+++ aulas/31-acessandooDOM/script.js
@@ -0,0 +1,15 @@
+//tagname
+var titulo= document.getElementsByTagName('h1')[0];
+
+console.log(titulo); 
+
+var li =document.getElementsByTagName('li');
+console.log(li);
+//id
+var paragrafo=document.getElementById('paragrafo');
+console.log(paragrafo);
+
+//classe
+
+var itens=document.getElementsByClassName('item');
+console.log(itens);
