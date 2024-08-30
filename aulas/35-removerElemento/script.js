dede35bb1e111f1bb43f0cabd6bc644467e3bc49
JairoCatumbo <jairoreceadocatumbo@gmail.com>
2024-08-30 22:38:25 +0100

    35-removendoElemento
    removendo elementos no html:
    pelo removeChild;
    pelo remove()

---
+++ aulas/35-removerElemento/script.js
@@ -0,0 +1,10 @@
+var container= document.querySelector("#container");
+
+//removeChild
+
+var p= document.querySelector("#container p")
+
+container.removeChild(p);
+
+var subtitle=document.querySelector(".subtitle");
+subtitle.remove();
