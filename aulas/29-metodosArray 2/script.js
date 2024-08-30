d52a2c9a4abb3a364b5d6ad61b95f387e159a87e
JairoCatumbo <jairoreceadocatumbo@gmail.com>
2024-08-30 20:56:36 +0100

    29-metodosArray2
    mas metodos para manipular arrays

---
+++ aulas/29-metodosArray 2/script.js
@@ -0,0 +1,20 @@
+var arr=[1,2,3,4,5];
+
+arr.splice(2,0,999);
+console.log(arr);
+
+arr.splice(4,1);
+
+console.log(arr);
+
+console.log(arr.indexOf(999));
+
+var arr2=["o","jairo","eh","o","cara"];
+
+var frase=arr2.join(" ");
+
+console.log(frase);
+
+//enverter a ordem do array
+
+console.log(arr2.reverse());
