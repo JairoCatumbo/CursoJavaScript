4b1fc16c60a743c6c8704bb8698bc002e1d498df
JairoCatumbo <jairoreceadocatumbo@gmail.com>
2024-08-31 13:29:40 +0100

    39-adicionandoCSS
    adicinando elemento css pelo js:
    selecciona o elemento;
    el.style.color="red";
    el.style.cssText="color:red; background-color:blue;";

---
+++ aulas/39-adicionandoCSS/script.js
@@ -0,0 +1,9 @@
+var title=document.querySelector("#title");
+
+title.style.color="red";
+
+title.style.backgroundColor="yellow";
+
+var sub=document.querySelector(".subtitle");
+
+sub.style.cssText="color: blue; background-color:pink; font-size:20pt;";
